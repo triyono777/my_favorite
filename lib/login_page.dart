63d1a39d85def9af42fff851074a0666e33ea110
triyono <yono.tri@gmail.com>
2020-10-19 21:34:17 +0700

    add button

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -6,8 +6,38 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController textEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Login Page'),),);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Page'),
+      ),
+      body: Column(
+        children: [
+          Text('Selamat'),
+          TextField(
+            onChanged: (val){
+              setState(() {
+
+              });
+            },
+            controller: textEditingController,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+            ),
+          ),
+          textEditingController.text.isNotEmpty
+              ? RaisedButton(
+                  onPressed: () {},
+                  child: Text('Lanjut'),
+                )
+              : SizedBox()
+        ],
+      ),
+    );
   }
 }
